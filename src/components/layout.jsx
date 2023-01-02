87d596ae4daf6249cd2caaf7280ebaf6ff9785c1
Carlos19979 <carloscode23@gmail.com>
2023-01-02 17:24:31 +0100

    update

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,7 +3,7 @@ import { RiDashboardFill } from "react-icons/ri";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen min-w-screen">
+    <div className="min-h-screen min-w-screen mx-8 my-8 ">
       <div className="grid grid-cols-6">
         <div className="col-span-1">
           <div className="text-center p8">
