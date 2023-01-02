cefb8ea06a88433e031b4d91eb9959c2cb6b41a9
gmluqa <gmluqa@protonmail.com>
2023-01-02 19:49:34 +0200

    feat: component register intergrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <div className="App ">
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
