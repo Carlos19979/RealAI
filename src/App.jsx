5c41610533aa51be1735221573763c856d776811
Carlos19979 <carloscode23@gmail.com>
2023-01-02 17:21:48 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import './App.css'
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
 
 function App() {
-      
   return (
     <div className="App ">
       <Routes>
-        <Route path="/" element={<Layout/>} />
+        <Route path="/" element={<Layout />} />
       </Routes>
     </div>
   );
 }
 
-
-export default App
+export default App;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 import { RiDashboardFill } from "react-icons/ri";
 
 export default function Layout() {
-   
-  
-    return (
-     <div className='min-h-screen grid grid-cols-6'>
-        <div className='bg-green-300'>hola</div>
-        <div className='bg-blue-400'>hola</div>
-     </div>
-    );
+  return (
+    <div className="min-h-screen min-w-screen">
+      <div className="grid grid-cols-6">
+        <div className="col-span-1">
+          <div className="text-center p8">
+            <h1 className="uppercase font-bold"> tu logo</h1>
+          </div>
+        </div>
+        <div className="col-span-5">hola2</div>
+      </div>
+    </div>
+  );
 }
